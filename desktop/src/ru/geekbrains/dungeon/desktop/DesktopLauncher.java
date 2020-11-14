ba99d639da06484dacb3d1eb678bc5a53c7dcd79
evvol <Keshavoloshin@mail.ru>
2020-11-14 11:55:37 +0300

    Create project

---
+++ desktop/src/ru/geekbrains/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.dungeon.DungeonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonGame(), config);
+	}
+}
