fec6928619f13af12cf52e6a2dbcb185552bc7eb
evvol <Keshavoloshin@mail.ru>
2020-11-21 15:13:51 +0300

    Class work unit4

--- core/src/ru/geekbrains/dungeon/units/Hero.java
+++ core/src/ru/geekbrains/dungeon/units/Hero.java
@@ -13,96 +13,23 @@ import ru.geekbrains.dungeon.GameMap;
 
 
 public class Hero extends Unit {
-    float movementTime;
-    float movementMaxTime;
-    int targetX, targetY;
-    private int experience;
-    private BitmapFont font;
-
-
     public Hero(TextureAtlas atlas, GameController gc) {
         super(gc, 1, 1, 10);
+        this.hpMax = 100;
+        this.hp = this.hpMax;
         this.texture = atlas.findRegion("knight");
         this.textureHp = atlas.findRegion("hp");
-        this.movementMaxTime = 0.2f;
-        this.targetX = cellX;
-        this.targetY = cellY;
-        this.experience = 0;
-        this.font = new BitmapFont();
-
-
     }
 
     public void update(float dt) {
-        checkMovement(dt);
-    }
-
-    public boolean isStayStill() {
-        return cellY == targetY && cellX == targetX;
-    }
-
-    public void checkMovement(float dt) {
-        if (Gdx.input.justTouched() && isStayStill()) {
-            if (Math.abs(gc.getCursorX() - cellX) + Math.abs(gc.getCursorY() - cellY) == 1) {
-                targetX = gc.getCursorX();
-                targetY = gc.getCursorY();
+        super.update(dt);
+        if (Gdx.input.justTouched() && canIMakeAction()) {
+            Monster m = gc.getUnitController().getMonsterController().getMonsterInCell(gc.getCursorX(), gc.getCursorY());
+            if (m != null && canIAttackThisTarget(m)) {
+                attack(m);
+            } else {
+                goTo(gc.getCursorX(), gc.getCursorY());
             }
         }
-
-        Monster m = gc.getMonsterController().getMonsterInCell(targetX, targetY);
-        if (m != null) {
-            targetX = cellX;
-            targetY = cellY;
-            if (m.takeDamage(1)) experience++;
-            changeStepCounter();
-
-        }
-
-        if (!gc.getGameMap().isCellPassable(targetX, targetY)) {
-            targetX = cellX;
-            targetY = cellY;
-        }
-
-        if (!isStayStill()) {
-            movementTime += dt;
-            if (movementTime > movementMaxTime) {
-                movementTime = 0;
-                cellX = targetX;
-                cellY = targetY;
-                changeStepCounter();
-
-            }
-        }
-
-        if (stepCounter <= 0) stepCounter = 5;
-    }
-
-    @Override
-    public void render(SpriteBatch batch) {
-        float px = cellX * GameMap.CELL_SIZE;
-        float py = cellY * GameMap.CELL_SIZE;
-        if (!isStayStill()) {
-            px = cellX * GameMap.CELL_SIZE + (targetX - cellX) * (movementTime / movementMaxTime) * GameMap.CELL_SIZE;
-            py = cellY * GameMap.CELL_SIZE + (targetY - cellY) * (movementTime / movementMaxTime) * GameMap.CELL_SIZE;
-        }
-
-        batch.draw(texture, px, py);
-        batch.setColor(0.0f, 0.0f, 0.0f, 1.0f);
-        batch.draw(textureHp, px + 1, py + 51, 58, 10);
-        batch.setColor(0.7f, 0.0f, 0.0f, 1.0f);
-        batch.draw(textureHp, px + 2, py + 52, 56, 8);
-        batch.setColor(0.0f, 1.0f, 0.0f, 1.0f);
-        batch.draw(textureHp, px + 2, py + 52, (float) hp / hpMax * 56, 8);
-        batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-        font.setColor(Color.RED);
-        font.draw(batch, "StepCounter " + stepCounter, px, py + 75);
-        font.setColor(Color.YELLOW);
-        font.draw(batch, "Score " + experience, px, py + 87);
-
-
-    }
-
-    public void disposeFont() {
-        font.dispose();
     }
 }
--- core/src/ru/geekbrains/dungeon/units/Monster.java
+++ core/src/ru/geekbrains/dungeon/units/Monster.java
@@ -6,36 +6,58 @@ import ru.geekbrains.dungeon.GameController;
 import java.util.Random;
 
 public class Monster extends Unit {
-    int lastHp;
-    Random random;
-    int damage;
-
-    public boolean isActive() {
-        return hp > 0;
-    }
+    private float aiBrainsImplseTime;
+    private Unit target;
 
     public Monster(TextureAtlas atlas, GameController gc) {
         super(gc, 5, 2, 10);
         this.texture = atlas.findRegion("monster");
         this.textureHp = atlas.findRegion("hp");
         this.hp = -1;
-        this.random = new Random();
-        this.damage = 1;
     }
 
     public void activate(int cellX, int cellY) {
         this.cellX = cellX;
         this.cellY = cellY;
+        this.targetX = cellX;
+        this.targetY = cellY;
         this.hpMax = 10;
         this.hp = hpMax;
-        this.lastHp = hpMax;
+        this.target = gc.getUnitController().getHero();
     }
 
     public void update(float dt) {
-        if (lastHp > hp && (Math.abs(gc.getHero().cellX - cellX) + Math.abs(gc.getHero().cellY - cellY)) == 1 && random.nextInt(100) < 25) {
-            gc.getHero().takeDamage(damage);
+        super.update(dt);
+        if (canIMakeAction()) {
+            if (isStayStill()) {
+                aiBrainsImplseTime += dt;
+            }
+            if (aiBrainsImplseTime > 0.4f) {
+                aiBrainsImplseTime = 0.0f;
+                if (canIAttackThisTarget(target)) {
+                    attack(target);
+                } else {
+                    tryToMove();
+                }
+            }
         }
-        lastHp = hp;
+    }
 
+    public void tryToMove() {
+        int bestX = -1, bestY = -1;
+        float bestDst = 10000;
+        for (int i = cellX - 1; i <= cellX + 1; i++) {
+            for (int j = cellY - 1; j <= cellY + 1; j++) {
+                if (Math.abs(cellX - i) + Math.abs(cellY - j) == 1 && gc.getGameMap().isCellPassable(i, j) && gc.getUnitController().isCellFree(i, j)) {
+                    float dst = (float) Math.sqrt((i - target.getCellX()) * (i - target.getCellX()) + (j - target.getCellY()) * (j - target.getCellY()));
+                    if (dst < bestDst) {
+                        bestDst = dst;
+                        bestX = i;
+                        bestY = j;
+                    }
+                }
+            }
+        }
+        goTo(bestX, bestY);
     }
 }
